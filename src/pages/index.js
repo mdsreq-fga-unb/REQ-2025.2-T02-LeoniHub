1b3255096c40ef95fb62e03cd490332149ad7c6b
KauaVL <kauavaleleao@gmail.com>
2025-09-22 21:45:28 -0300

    fix: corrige links quebrados para Cenário Atual e ajusta navegação

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+              to="/docs/visao_produto/cenario_atual">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
