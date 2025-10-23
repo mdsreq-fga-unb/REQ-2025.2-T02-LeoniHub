0b01f34a655280ef8aa6e2011151bfb31b39901a
KauaVL <kauavaleleao@gmail.com>
2025-10-23 20:18:11 -0300

    corrigindo ajuste no nome das rotas

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,19 +1,19 @@
-import { supabaseLoja1 } from './db_loja1.js';
-import { supabaseLoja2 } from './db_loja2.js';
+import { supabaseClosetChic } from './closetChic.js';
+import { supabaseLeonni } from './leonni.js';
 
 export const getSupabaseClient = (lojaId) => {
   switch (lojaId) {
-    case 'loja1':
+    case 'ClosetChic':
     case '1':
-      if (!supabaseLoja1) throw new Error('Loja 1 não configurada');
-      return supabaseLoja1;
-    case 'loja2':
+      if (!supabaseClosetChic) throw new Error('ClosetChic não configurada');
+      return supabaseClosetChic;
+    case 'Leonni':
     case '2':
-      if (!supabaseLoja2) throw new Error('Loja 2 não configurada. Configure SUPABASE_LOJA2_URL e SUPABASE_LOJA2_ANON_KEY no .env');
-      return supabaseLoja2;
+      if (!supabaseLeonni) throw new Error('Leonni não configurada. Configure SUPABASE_LEONNI_URL e SUPABASE_LEONNI_ANON_KEY no .env');
+      return supabaseLeonni;
     default:
-      throw new Error(`Loja inválida: ${lojaId}. Use: loja1 ou loja2`);
+      throw new Error(`Loja inválida: ${lojaId}. Use: ClosetChic ou Leonni`);
   }
 };
 
-export { supabaseLoja1, supabaseLoja2 };
+export { supabaseClosetChic, supabaseLeonni };
