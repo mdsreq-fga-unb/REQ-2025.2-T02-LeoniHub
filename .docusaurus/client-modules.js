38dd6aea3241d011d1174958b508d2540a6588af
Diih <diogocorjesu@gmail.com>
2025-11-14 10:53:19 -0300

    Remove docusauras e node_modules

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/diogo/Coding/Faculdade/REQ-2025.2-T02-LeoniHub/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/diogo/Coding/Faculdade/REQ-2025.2-T02-LeoniHub/node_modules/infima/dist/css/default/default.css"),
-  require("/home/diogo/Coding/Faculdade/REQ-2025.2-T02-LeoniHub/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/diogo/Coding/Faculdade/REQ-2025.2-T02-LeoniHub/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/diogo/Coding/Faculdade/REQ-2025.2-T02-LeoniHub/src/css/custom.css"),
-];
